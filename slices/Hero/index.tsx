060fe84f761e4ca15462e19e3102be98eb3f4034
nabesteron <49988463+nabesteron@users.noreply.github.com>
2024-01-14 18:12:24 -0500

    created Bounded.tsx as a helper component

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -1,9 +1,9 @@
-"use client";
-
-import { useRef, useEffect } from "react";
+'use client'
+import { useEffect, useRef } from "react";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import gsap from "gsap";
+
 /**
  * Props for `Hero`.
  */
@@ -16,25 +16,44 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
   const component = useRef(null);
 
   useEffect(() => {
-    let ctx = gsap.context(() => {
-      const tl = gsap.timeline();
-      tl.fromTo(
-        ".name-animation",
-        {
-          x: -100,
-          opacity: 0,
-          rotate: -10,
+    const tl = gsap.timeline();
+    tl.fromTo(
+      ".name-animation",
+      {
+        x: -100,
+        opacity: 0,
+        rotate: -10,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        rotate: 0,
+        duration: 1,
+        ease: "elastic.out(1,0.3)",
+        transformOrigin: "left top",
+        delay: 0.5,
+        stagger: {
+          each: 0.07,
+          from: "random",
         },
+      }
+    );
 
-        {
-          x: 0,
-          opacity: 1,
-          rotate: 0,
-        }
-      );
-    }, component);
-
-    return () => ctx.revert();
+    tl.fromTo(
+      ".job-title",
+      {
+        y: 20,
+        opacity: 0,
+        scale: 1.2,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        scale: 1,
+        ease: "elastic.out(1,0.3)",
+      }
+    );
   }, []);
 
   const renderLetters = (name: KeyTextField, key: string) => {
@@ -51,8 +70,8 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
     <section
       data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}ref={component}>
-      
+      data-slice-variation={slice.variation}
+      ref={component}>
       <div className='grid-min-h-[70vh] grid-col-1 md:grid-col-2 items-center'>
         <div className='col-start-1 md:row-start-1'>
           <h1
@@ -68,7 +87,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
               {renderLetters(slice.primary.last_name, "last")}
             </span>
           </h1>
-          <span className='block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl'>
+          <span className='job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl'>
             {slice.primary.tag_line}
           </span>
         </div>
